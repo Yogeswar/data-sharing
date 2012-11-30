3415efb2ea9e6529270c362839fa95b01ef95a63
manasbapna <manasbapna@gmail.com>
2012-11-30 06:29:59 +0000

    file splitter updated

---
+++ src/my/FileSplitter/FileManager.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class FileManager {
+	
+	final static int CHUNKSIZE=100000;
+	
+	public static String getFileExtension(String path) {
+		
+		  Pattern p = Pattern.compile(".*?([^\\\\/]+)$");  
+	      Matcher m = p.matcher(path);  
+	      String filename=((m.find()) ? m.group(1) : "");
+	      
+	      String[] tokens = filename.split("\\.");
+	      String[] a = new String[2];
+	      
+	      for(int i=0;i<tokens.length;i++){
+	    	  a[i]=tokens[i];
+	      }
+		return a[1];
+	}
+
+	public static String getFileName(String path){
+		
+		  Pattern p = Pattern.compile(".*?([^\\\\/]+)$");  
+	      Matcher m = p.matcher(path);  
+	      String filename=((m.find()) ? m.group(1) : "");
+	      
+	      String[] tokens = filename.split("\\.");
+	      String[] a = new String[2];
+	      
+	      for(int i=0;i<tokens.length;i++){
+	    	  a[i]=tokens[i];
+	      }
+	      return a[0];
+	}
+	
+	public static int getFileSize(String path){
+		File file = new File ( path );
+		  int FILE_SIZE = (int) file.length();
+		  System.out.println("Total File Size: "+FILE_SIZE);
+		  return FILE_SIZE;
+		  
+	}
+	
+	public static int getNumberOfParts(String path){
+		int parts= (getFileSize(path)/CHUNKSIZE) + 1;
+		System.out.println("Number of Parts : "+ parts);
+		return parts;
+	}
+
+}
